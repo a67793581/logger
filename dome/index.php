0af0161a1e345249203cf0ab15079a50d809a4fd
jiangbj <jiangbj@kongdao.me>
2019-05-10 20:51:38 +0800

    初始化

---
+++ dome/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once '../vendor/autoload.php';
+
+use \logger\Logger;
+
+#设置logs根目录
+define('LOG_PATH', realpath(__DIR__).DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR); // 日志目录
+Logger::i('测试字符串内容', '中文路径名/测试文件', '0');
+Logger::i(array('数组内容'), 'array/res', '1');
+Logger::i(array('数组内容'), 'array/res', '2');
